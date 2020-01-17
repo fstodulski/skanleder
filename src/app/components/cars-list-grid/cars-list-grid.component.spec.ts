1e56e82f4c77fb0f12f1102c2c0484d10c831e1b
Franek <franek.stodulski@gmail.com>
2020-01-17 23:45:39 +0100

    feat: added config for gcloud

--- src/app/components/cars-list-grid/cars-list-grid.component.spec.ts
+++ src/app/components/cars-list-grid/cars-list-grid.component.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RecentAddedCarsComponent } from './cars-list-grid.component';
+import { CarsListGridComponent } from './cars-list-grid.component';
 
-describe('RecentAddedCarsComponent', () => {
-  let component: RecentAddedCarsComponent;
-  let fixture: ComponentFixture<RecentAddedCarsComponent>;
+describe('CarsListGridComponent', () => {
+  let component: CarsListGridComponent;
+  let fixture: ComponentFixture<CarsListGridComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [RecentAddedCarsComponent],
+      declarations: [CarsListGridComponent],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RecentAddedCarsComponent);
+    fixture = TestBed.createComponent(CarsListGridComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
