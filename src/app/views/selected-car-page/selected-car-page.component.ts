3e741d569909fc718e7c8862c7cd1c79e1eb8f44
Franek <franek.stodulski@gmail.com>
2020-01-17 22:33:14 +0100

    fix: removed errors for null

--- src/app/views/selected-car-page/selected-car-page.component.ts
+++ src/app/views/selected-car-page/selected-car-page.component.ts
@@ -15,21 +15,20 @@ import { CarService } from '../../services/car/car.service';
         <span class="app-selected-car-page__go-back" routerLink="/wykonane-prace">
           Wróć do listy
         </span>
-
-        <section class="app-selected-car-page__car-details">
+        <section class="app-selected-car-page__car-details" *ngIf="selectedCarData$ | async">
           <article class="car-details">
             <h1 class="car-details__name">
-              {{ (selectedCarData$ | async).name }}
+              {{ (selectedCarData$ | async)?.name }}
             </h1>
             <p class="car-details__category">
-              {{ (selectedCarData$ | async).carCategoryName.carCategoryName }}
+              {{ (selectedCarData$ | async)?.carCategoryName.carCategoryName }}
             </p>
           </article>
         </section>
 
         <section class="app-selected-car-page__images-presentation">
           <app-selected-car-images-presentation
-            [carPhotos]="(selectedCarData$ | async).photos"
+            [carPhotos]="(selectedCarData$ | async)?.photos"
           ></app-selected-car-images-presentation>
         </section>
       </app-content-container>
